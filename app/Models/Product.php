684d1071a4d0e0ca1631cebcfbc0a0b2de516fcd
Oxcode <samuel@tedbree.com>
2025-08-07 17:32:52 +0100

    initial commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,11 +2,54 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    /** @use HasFactory<\Database\Factories\ProductFactory> */
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'title',
+        'author',
+        'description',
+        'content',
+        'url',
+        'image',
+        'source',
+        'category_id' 
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->BelongsTo(Category::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function wishlists()
+    {
+        return $this->hasMany(Wishlist::class);
+    }
+
+    public static function search($query)
+    {
+        $relations = ['comments', 'category'];
+
+        return empty($query) ? static::query()->with($relations)
+            : static::with($relations)
+                ->where('title', 'like', '%'.$query.'%')
+                ->orWhere('source', 'like', '%'.$query.'%')
+                ->orWhere('author', 'like', '%'.$query.'%');
+    }
 }
