cb074716c5caa6506d6a77fcafcea070cf2491a2
Oxcode <samuel@tedbree.com>
2025-08-08 22:52:59 +0100

    elite updates

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,26 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::prefix('categories')->group(function() {
+        Route::get('/', [CategoryController::class, 'index'])->name('categories');
+        Route::get('/create', [CategoryController::class, 'create'])->name('categories.create');
+        Route::post('/store', [CategoryController::class, 'store'])->name('categories.store');
+        Route::get('/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+        Route::delete('/{category}/delete', [CategoryController::class, 'delete'])->name('categories.delete');
+        Route::get('/{category}', [CategoryController::class, 'view'])->name('categories.view');
+    });
+
+    Route::prefix('products')->group(function() {
+        Route::get('/', [ProductController::class, 'index'])->name('products');
+        Route::get('/create', [ProductController::class, 'create'])->name('products.create');
+        Route::post('/store', [ProductController::class, 'store'])->name('products.store');
+        Route::get('/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+        Route::delete('/{product}/delete', [ProductController::class, 'delete'])->name('products.delete');
+        Route::get('/{product}', [ProductController::class, 'view'])->name('products.view');
+    });
 });
 
-Route::prefix('categories')->group(function() {
-    Route::get('/', [CategoryController::class, 'index'])->name('categories');
-    Route::get('/create', [CategoryController::class, 'create'])->name('categories.create');
-    Route::post('/store', [CategoryController::class, 'store'])->name('categories.store');
-    Route::get('/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-    Route::delete('/{category}/delete', [CategoryController::class, 'delete'])->name('categories.delete');
-    Route::get('/{category}', [CategoryController::class, 'view'])->name('categories.view');
-})->middleware(['auth', 'verified']);
-
-Route::prefix('products')->group(function() {
-    Route::get('/', [ProductController::class, 'index'])->name('products');
-    Route::get('/create', [ProductController::class, 'create'])->name('products.create');
-    Route::post('/store', [ProductController::class, 'store'])->name('products.store');
-    Route::get('/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-    Route::delete('/{product}/delete', [ProductController::class, 'delete'])->name('products.delete');
-    Route::get('/{product}', [ProductController::class, 'view'])->name('products.view');
-})->middleware(['auth', 'verified']);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
