5d3a3479f22355bcfab948fb1b6d6a37d6050ef6
Oxcode <samuel@tedbree.com>
2025-08-09 11:32:00 +0100

    elite updates

--- resources/js/pages/products/show.tsx
+++ resources/js/pages/products/show.tsx
@@ -62,7 +62,7 @@ export default function Dashboard() {
                             </div>
                             <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt className="text-sm font-medium text-gray-500 dark:text-white">Price</dt>
-                                <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{ product?.price || '' }</dd>
+                                <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">$ { product?.price || '' }</dd>
                             </div>
                             <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt className="text-sm font-medium text-gray-500 dark:text-white">Stock (Unit)</dt>
