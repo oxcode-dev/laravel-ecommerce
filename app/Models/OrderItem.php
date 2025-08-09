d0c686373b025f6779d4536a2096fa134fea484a
Oxcode <samuel@tedbree.com>
2025-08-09 15:45:52 +0100

    elite updates

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -4,9 +4,45 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrderItem extends Model
 {
     /** @use HasFactory<\Database\Factories\OrderItemFactory> */
     use HasFactory;
+
+
+    // $table->id();
+    // $table->foreignUuid('order_id');
+    // $table->foreignUuid('product_id');
+    // $table->integer('quantity')->default(0);
+    // $table->decimal('unit_price', 10, 2)->default(0.00);
+    // $table->timestamps();
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity',
+        'unit_price',
+    ];
+
+    public function order() :BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product() :BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public static function search($query)
+    {
+        $relations = ['order', 'product'];
+
+        return empty($query) ? static::query()//    ->with($relations)
+            : static::with($relations)
+                ->where('name', 'like', '%'.$query.'%')
+                ->orWhere('slug', 'like', '%'.$query.'%');
+    }
 }
