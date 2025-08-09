c49c3117b76d2f0fbd6f739e701af60f92aa28da
Oxcode <samuel@tedbree.com>
2025-08-09 17:27:07 +0100

    elite updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Address;
 use App\Models\Category;
+use App\Models\Order;
+use App\Models\OrderItem;
 use App\Models\Product;
+use App\Models\Review;
 use App\Models\User;
+use App\Models\Wishlist;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -25,5 +30,10 @@ class DatabaseSeeder extends Seeder
 
         Category::factory(3)->create();
         Product::factory(10)->create();
+        Order::factory(4)->create();
+        Review::factory(4)->create();
+        Wishlist::factory(4)->create();
+        OrderItem::factory(4)->create();
+        Address::factory(4)->create();
     }
 }
