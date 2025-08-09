453d45169979027b21aff1cf17fce2aac9b36cd5
Oxcode <samuel@tedbree.com>
2025-08-09 01:40:21 +0100

    elite updates

--- resources/js/components/Pager.tsx
+++ resources/js/components/Pager.tsx
@@ -12,25 +12,30 @@ export default function Pager({prev_page_url, next_page_url, last_page, current_
         <>
             <div className='flex items-center justify-center'>
                 <div className="flex justify-center items-center space-x-4">
-                    <div className="border rounded-md bg-gray-100 px-2 py-1 text-3xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm">
-                        <Link href="prev_page_url">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-                            </svg>
-                        </Link>
-                    </div>
+                    {
+                        prev_page_url ? 
+                        <div className="border rounded-md bg-gray-100 px-2 py-1 text-3xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm">
+                            <Link href={prev_page_url}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                                </svg>
+                            </Link>
+                        </div> : null
+                    }
                     <div className="text-slate-500">
                         <span className="font-bold text-lg">{ `${current_page} ` }</span>
                         <span>{ ` / ${last_page}` }</span>
-                        
                     </div>
-                    <div className="border rounded-md bg-gray-100 px-2 py-1 text-3xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm">
-                        <Link href="next_page_url">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                            </svg>
-                        </Link>
-                    </div>
+                    {
+                        next_page_url ? 
+                        <div className="border rounded-md bg-gray-100 px-2 py-1 text-3xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm">
+                            <Link href={next_page_url}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                                </svg>
+                            </Link>
+                        </div> : null
+                    }
                 </div>
             </div>
         </>
