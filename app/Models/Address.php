76d97e43bfb2ab42c132ee292b184dadf36a97c7
Oxcode <samuel@tedbree.com>
2025-08-09 16:18:24 +0100

    elite updates

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,11 +2,45 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Address extends Model
 {
     /** @use HasFactory<\Database\Factories\AddressFactory> */
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'user_id',
+        'street',
+        'city',
+        'state',
+        'country',
+        'postal_code',
+        'is_default',
+    ];
+
+    public function user() :BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public static function search($query)
+    {
+        $relations = ['user'];
+
+        return empty($query) ? static::query()//    ->with($relations)
+            : static::with($relations)
+                ->where('name', 'like', '%'.$query.'%')
+                ->orWhere('slug', 'like', '%'.$query.'%');
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'is_default' => 'boolean',
+        ];
+    }
 }
