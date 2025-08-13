72ba3b1e7ec5bef8a116425998db50c6d2d2dff3
Oxcode <samuel@tedbree.com>
2025-08-13 15:25:55 +0100

    elite updates

--- app/Http/Requests/Settings/ProfileUpdateRequest.php
+++ app/Http/Requests/Settings/ProfileUpdateRequest.php
@@ -27,6 +27,7 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+            'phone' => ['required', 'string', 'max:255'],
         ];
     }
 }
