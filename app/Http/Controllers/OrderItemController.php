b575f7aea5480fc1ee78c12a51270d27076c7e27
Oxcode <samuel@tedbree.com>
2025-08-18 22:29:06 +0100

    elite updates

--- app/Http/Controllers/OrderItemController.php
+++ app/Http/Controllers/OrderItemController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\OrderItem;
 use Illuminate\Http\Request;
 
 class OrderItemController extends Controller
 {
     public function index() 
     {
-        
+        $orderItems = OrderItem::with('product.user')->whereHas('product', function ($query) {
+            $query->where('user_id', auth()->user()->id);
+        })->get();
+        dd($orderItems->toArray());
     }
 }
---
+++ resources/js/pages/order-items/index.tsx
@@ -0,0 +1,76 @@
+import Pager from '@/components/Pager';
+import { formatDate } from '@/helper';
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem, OrderType, OrderItem } from '@/types';
+import { Head, usePage } from '@inertiajs/react';
+import { Link } from '@inertiajs/react'
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Orders',
+        href: '/orders',
+    },
+];
+
+export default function Dashboard() {
+    // @ts-ignore
+    const orders: OrderType = usePage().props.orders
+    const ordersData: OrderItem[] = orders?.data || {}
+
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Orders" />
+
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
+            <div className="flex justify-end px-4">
+                <Link href="/products/create" className="bg-blue-600 text-white rounded-lg px-4 py-2">
+                    Create
+                </Link>
+            </div>
+            <div className="relative min-h-[100vh] rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                
+                <div>
+                    <table className="min-w-full bg-transparent">
+                        <thead>
+                            <tr className="bg-gray-500 text-white border-b">
+                                <th className="py-3 px-4 text-left">User</th>
+                                <th className="py-3 px-4 text-left">Date</th>
+                                <th className="py-3 px-4 text-left">Total($)</th>
+                                <th className="py-3 px-4 text-left">Status</th>
+                                <th className="py-3 px-4 text-left">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody className="">
+                            {
+                                ordersData.map((order, key) => (
+                                    <tr key={key} className="border-b border-blue-gray-200 capitalize">
+                                        <td className="py-3 px-4">{ order?.user?.name || '' }</td>
+                                        <td className="py-3 px-4">{ formatDate(order?.created_at || null) }</td>
+                                        <td className="py-3 px-4 capitalize">{ order?.total_amount || 0 }</td>
+                                        <td className="py-3 px-4">{ order?.status }</td>
+                                        <td className="py-3 px-4">
+                                            <Link href={`/orders/${order.id}`} className="font-medium text-blue-600 hover:text-blue-800">
+                                                View
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                            
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className="py-6">
+                    <Pager 
+                        prev_page_url={orders?.prev_page_url}
+                        next_page_url={orders?.next_page_url}
+                        last_page={orders?.last_page}
+                        current_page={orders?.current_page}
+                    />
+                </div>
+            </div>
+        </div>
+        </AppLayout>
+    )
+};
