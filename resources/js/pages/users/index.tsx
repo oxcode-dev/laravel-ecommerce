f73c0081b0a80ff617c3b2b99e2b8863830addf2
Oxcode <samuel@tedbree.com>
2025-08-19 21:30:45 +0100

    elite updates

--- resources/js/pages/users/index.tsx
+++ resources/js/pages/users/index.tsx
@@ -1,5 +1,6 @@
 import Pager from '@/components/Pager';
 import CreateUser from '@/components/create-user';
+import EmptyState from '@/components/empty-state';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem, User as UserItem, UserType } from '@/types';
 import { Head, usePage } from '@inertiajs/react';
@@ -48,51 +49,56 @@ export default function Dashboard() {
                     }
                     
                 </div>
-                <div className="relative min-h-[100vh] rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    
-                    <div>
-                        <table className="min-w-full bg-transparent">
-                            <thead>
-                                <tr className="bg-gray-500 text-white border-b">
-                                    <th className="py-3 px-4 text-left">User</th>
-                                    <th className="py-3 px-4 text-left">Email</th>
-                                    { page_type === 'vendor' ? <th className="py-3 px-4 text-left">Products</th> : null }
-                                    { page_type === 'customer' ? <th className="py-3 px-4 text-left">Orders</th> : null }
-                                    <th className="py-3 px-4 text-left">Phone</th>
-                                    <th className="py-3 px-4 text-left">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody className="">
-                                {
-                                    usersData.map((user, key) => (
-                                        <tr key={key} className="border-b border-blue-gray-200 capitalize">
-                                            <td className="py-3 px-4">{ user?.name || '' }</td>
-                                            <td className="py-3 px-4 lowercase">{ user?.email || '' }</td>
-                                            { page_type === 'vendor' ? <th className="py-3 px-4 text-left">{ user?.products.length}</th> : null }
-                                            { page_type === 'customer' ? <th className="py-3 px-4 text-left">{ user?.orders.length}</th> : null }
-                                            <td className="py-3 px-4">{ user?.phone }</td>
-                                            <td className="py-3 px-4">
-                                                <Link href={`/users/${user.id}`} className="font-medium text-blue-600 hover:text-blue-800">
-                                                    View
-                                                </Link>
-                                            </td>
-                                        </tr>
-                                    ))
-                                }
-                                
-                            </tbody>
-                        </table>
-                    </div>
 
-                    <div className="py-6">
-                        <Pager 
-                            prev_page_url={users?.prev_page_url}
-                            next_page_url={users?.next_page_url}
-                            last_page={users?.last_page}
-                            current_page={users?.current_page}
-                        />
+                { usersData.length === 0 && <EmptyState title='No Data' />}
+
+                { usersData.length !== 0 &&
+                    <div className="relative min-h-[100vh] rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                        
+                        <div>
+                            <table className="min-w-full bg-transparent">
+                                <thead>
+                                    <tr className="bg-gray-500 text-white border-b">
+                                        <th className="py-3 px-4 text-left">User</th>
+                                        <th className="py-3 px-4 text-left">Email</th>
+                                        { page_type === 'vendor' ? <th className="py-3 px-4 text-left">Products</th> : null }
+                                        { page_type === 'customer' ? <th className="py-3 px-4 text-left">Orders</th> : null }
+                                        <th className="py-3 px-4 text-left">Phone</th>
+                                        <th className="py-3 px-4 text-left">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="">
+                                    {
+                                        usersData.map((user, key) => (
+                                            <tr key={key} className="border-b border-blue-gray-200 capitalize">
+                                                <td className="py-3 px-4">{ user?.name || '' }</td>
+                                                <td className="py-3 px-4 lowercase">{ user?.email || '' }</td>
+                                                { page_type === 'vendor' ? <th className="py-3 px-4 text-left">{ user?.products.length}</th> : null }
+                                                { page_type === 'customer' ? <th className="py-3 px-4 text-left">{ user?.orders.length}</th> : null }
+                                                <td className="py-3 px-4">{ user?.phone }</td>
+                                                <td className="py-3 px-4">
+                                                    <Link href={`/users/${user.id}`} className="font-medium text-blue-600 hover:text-blue-800">
+                                                        View
+                                                    </Link>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                    
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <div className="py-6">
+                            <Pager 
+                                prev_page_url={users?.prev_page_url}
+                                next_page_url={users?.next_page_url}
+                                last_page={users?.last_page}
+                                current_page={users?.current_page}
+                            />
+                        </div>
                     </div>
-                </div>
+                }
             </div>
         </AppLayout>
     )
