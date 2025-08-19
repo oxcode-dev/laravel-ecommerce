82b8c5e6246bb2a9932a150369b2540386f12ba4
Oxcode <samuel@tedbree.com>
2025-08-19 21:29:16 +0100

    elite updates

--- resources/js/pages/products/index.tsx
+++ resources/js/pages/products/index.tsx
@@ -1,4 +1,5 @@
 import Pager from '@/components/Pager';
+import EmptyState from '@/components/empty-state';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem, ProductType, ProductItem } from '@/types';
 import { Head, usePage } from '@inertiajs/react';
@@ -21,55 +22,60 @@ export default function Dashboard() {
             <Head title="Products" />
 
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-            <div className="flex justify-end px-4">
-                <Link href="/products/create" className="bg-blue-600 text-white rounded-lg px-4 py-2">
-                    Create
-                </Link>
-            </div>
-            <div className="relative min-h-[100vh] rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                
-                <div>
-                    <table className="min-w-full bg-transparent">
-                        <thead>
-                            <tr className="bg-gray-500 text-white border-b">
-                                <th className="py-3 px-4 text-left">Title</th>
-                                <th className="py-3 px-4 text-left">Category</th>
-                                <th className="py-3 px-4 text-left">Price($)</th>
-                                <th className="py-3 px-4 text-left">Owner</th>
-                                <th className="py-3 px-4 text-left">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody className="">
-                            {
-                                productsData.map((product, key) => (
-                                    <tr key={key} className="border-b border-blue-gray-200 capitalize">
-                                        <td className="py-3 px-4">{ product?.title || '' }</td>
-                                        <td className="py-3 px-4">{ product?.category?.name }</td>
-                                        <td className="py-3 px-4 capitalize">{ product?.price || '' }</td>
-                                        <td className="py-3 px-4">{ product?.user?.name }</td>
-                                        <td className="py-3 px-4">
-                                            <Link href={`/products/${product.id}`} className="font-medium text-blue-600 hover:text-blue-800">
-                                                View
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                ))
-                            }
-                            
-                        </tbody>
-                    </table>
+                <div className="flex justify-end px-4">
+                    <Link href="/products/create" className="bg-blue-600 text-white rounded-lg px-4 py-2">
+                        Create
+                    </Link>
                 </div>
 
-                <div className="py-6">
-                    <Pager 
-                        prev_page_url={products?.prev_page_url}
-                        next_page_url={products?.next_page_url}
-                        last_page={products?.last_page}
-                        current_page={products?.current_page}
-                    />
-                </div>
-            </div>
-        </div>
+                { productsData.length === 0 && <EmptyState title='No Data' />}
+
+                { productsData.length !== 0 &&
+                    <div className="relative min-h-[100vh] rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                        
+                        <div>
+                            <table className="min-w-full bg-transparent">
+                                <thead>
+                                    <tr className="bg-gray-500 text-white border-b">
+                                        <th className="py-3 px-4 text-left">Title</th>
+                                        <th className="py-3 px-4 text-left">Category</th>
+                                        <th className="py-3 px-4 text-left">Price($)</th>
+                                        <th className="py-3 px-4 text-left">Owner</th>
+                                        <th className="py-3 px-4 text-left">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="">
+                                    {
+                                        productsData.map((product, key) => (
+                                            <tr key={key} className="border-b border-blue-gray-200 capitalize">
+                                                <td className="py-3 px-4">{ product?.title || '' }</td>
+                                                <td className="py-3 px-4">{ product?.category?.name }</td>
+                                                <td className="py-3 px-4 capitalize">{ product?.price || '' }</td>
+                                                <td className="py-3 px-4">{ product?.user?.name }</td>
+                                                <td className="py-3 px-4">
+                                                    <Link href={`/products/${product.id}`} className="font-medium text-blue-600 hover:text-blue-800">
+                                                        View
+                                                    </Link>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                    
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <div className="py-6">
+                            <Pager 
+                                prev_page_url={products?.prev_page_url}
+                                next_page_url={products?.next_page_url}
+                                last_page={products?.last_page}
+                                current_page={products?.current_page}
+                            />
+                        </div>
+                    </div>
+                }
+            </div>  
         </AppLayout>
     )
 };
