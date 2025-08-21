457c2f99ccc0443fe1491b371b5b28a54ba608e1
Oxcode <samuel@tedbree.com>
2025-08-21 21:43:08 +0100

    elite updates

---
+++ app/Http/Controllers/Api/WishlistController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\API\BaseController;
+use App\Models\Wishlist;
+use Illuminate\Http\Request;
+
+class WishlistController extends BaseController
+{
+    public function index (Request $request) 
+    {
+        $user = $request->user();
+
+        $wishlists = Wishlist::search($request->get('search', ''))
+            ->where('user_id', $user->id)
+            ->orderBy(
+                $request->get('sortField', 'created_at'),
+                $request->get('sortAsc') === 'true' ? 'asc' : 'desc'
+            )    
+            ->paginate($request->get('perPage', 10));
+
+        return $this->sendResponse(
+            $wishlists,
+            'Products fetched successfully!!!.',
+        );
+    }
+
+    public function show (Request $request, Wishlist $wishlist) 
+    {
+        $user = $request->user();
+
+        $wishlist = Wishlist::search($request->get('search', ''))
+            ->where('user_id', $user->id)
+            ->whereId($wishlist->id)
+            ->first();
+
+        return $this->sendResponse(
+            $wishlist,
+            'Products fetched successfully!!!.',
+        );
+    }
+}
