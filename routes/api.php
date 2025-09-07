f1c366b55ed7929400206e9700d271866dfe7141
Oxcode <samuel@tedbree.com>
2025-09-08 00:03:42 +0100

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -55,4 +55,3 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/{review}', [ReviewController::class, 'show'])->name('api.reviews_show');
     })->middleware('auth:sanctum');
 });
-
