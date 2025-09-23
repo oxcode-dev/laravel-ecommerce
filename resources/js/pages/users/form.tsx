1a9da8850aeddf7bd714144cd854356b0a1e1909
oxcode <mrexcelsam1@live.com>
2025-09-23 23:43:11 +0100

    setup

--- resources/js/pages/users/form.tsx
+++ resources/js/pages/users/form.tsx
@@ -18,7 +18,8 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 type RegisterForm = {
-    name: string;
+    first_name: string;
+    last_name: string;
     email: string;
     phone: string;
 };
@@ -26,7 +27,8 @@ type RegisterForm = {
 export default function Dashboard() {
 
     const { data, setData, post, processing, errors, reset } = useForm<Required<RegisterForm>>({
-        name: '',
+        first_name: '',
+        last_name: '',
         email: '',
         phone: '',
     });
@@ -52,20 +54,37 @@ export default function Dashboard() {
                         <form className="flex flex-col gap-6" onSubmit={submit}>
                             <div className="grid gap-6">
                                 <div className="grid gap-2">
-                                    <Label htmlFor="name">Name</Label>
+                                    <Label htmlFor="first_name">First Name</Label>
                                     <Input
-                                        id="name"
+                                        id="first_name"
                                         type="text"
                                         required
                                         autoFocus
                                         tabIndex={1}
-                                        autoComplete="name"
-                                        value={data.name}
-                                        onChange={(e) => setData('name', e.target.value)}
+                                        autoComplete="first_name"
+                                        value={data.first_name}
+                                        onChange={(e) => setData('first_name', e.target.value)}
                                         disabled={processing}
-                                        placeholder="Full name"
+                                        placeholder="First name"
                                     />
-                                    <InputError message={errors.name} className="mt-2" />
+                                    <InputError message={errors.first_name} className="mt-2" />
+                                </div>
+
+                                <div className="grid gap-2">
+                                    <Label htmlFor="last_name">Last Name</Label>
+                                    <Input
+                                        id="last_name"
+                                        type="text"
+                                        required
+                                        autoFocus
+                                        tabIndex={1}
+                                        autoComplete="last_name"
+                                        value={data.last_name}
+                                        onChange={(e) => setData('last_name', e.target.value)}
+                                        disabled={processing}
+                                        placeholder="Last name"
+                                    />
+                                    <InputError message={errors.last_name} className="mt-2" />
                                 </div>
 
                                 <div className="grid gap-2">
