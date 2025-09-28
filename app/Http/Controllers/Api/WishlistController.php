52528a6b96ee68c87fe29595edf8df7227fe4f29
oxcode <mrexcelsam1@live.com>
2025-09-28 17:30:34 +0100

    setup

--- app/Http/Controllers/Api/WishlistController.php
+++ app/Http/Controllers/Api/WishlistController.php
@@ -24,7 +24,7 @@ class WishlistController extends BaseController
                 $request->get('sortField', 'created_at'),
                 $request->get('sortAsc') === 'true' ? 'asc' : 'desc'
             )    
-            ->paginate($request->get('perPage', 1));
+            ->paginate($request->get('perPage', 2));
 
         return $this->sendResponse(
             // WishlistResource::collection($wishlists),
