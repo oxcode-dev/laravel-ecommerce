ab12faa35e65b0c6329ee334d30410408742fd07
oxcode <mrexcelsam1@live.com>
2025-10-11 00:52:31 +0100

    setup

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -44,9 +44,21 @@ class OrderController extends BaseController
 
     public function store(Request $request)
     {
+// cart
+// [{product_id: "880945c8-eb32-34c0-8e82-f8ae790bc33a", quantity: 4},…]
+
         // return $request->all();
         $validator = Validator::make($request->all(), [
-            'address_id' => ['string', 'uuid', 'required', 'exists:addresses,id']
+            'address_id' => ['string', 'uuid', 'required', 'exists:addresses,id'],
+            'name_on_card' => ['string', 'max:200', 'min:2', 'required'],
+            'card_number' => ['string', 'max:15', 'min:10', 'required'],
+            'cvv' => ['integer', 'max:3', 'min:3', 'required'],
+            'expiry_month' => ['integer', 'max:2', 'min:1', 'required'],
+            'expiry_year' => ['integer', 'max:4', 'min:2', 'required'],
+            // 'shippingCost' => ['integer', 'required'],
+            // 'tax' => ['integer', 'required'],
+            // 'totalAmount' => ['integer', 'required'],
+            // 'totalPrice' => ['integer', 'required'],
         ]);
 
         if($validator->fails()){
