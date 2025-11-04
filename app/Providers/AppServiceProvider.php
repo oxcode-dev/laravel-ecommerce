2963b448da25bdec8d1037d7c6d50d91e98fd297
oxcode <mrexcelsam1@live.com>
2025-11-04 13:22:40 +0100

    setup

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        JsonResource::withoutWrapping();
+
+        RateLimiter::for('api', function (Request $request) {
+            if(app()->isLocal()) {
+                return;
+            }
+            return Limit::perMinute(10)
+                ->by(optional($request->user())?->id ?: $request->ip());
+        });
+    }
+}
