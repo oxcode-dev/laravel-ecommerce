d92487ef131d252b7f46edcadc93c2b7083ef968
oxcode <mrexcelsam1@live.com>
2025-11-06 23:54:20 +0100

    setup

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -8,6 +8,7 @@ use App\Models\OrderItem;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Cache;
 
 class OrderController extends BaseController
 {
